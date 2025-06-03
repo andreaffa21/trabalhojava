1ef6442b136c5d6c403d42c6c897bbd7c52b7d94
andreaffa21 <elcymercedes@hotmail.com>
2025-06-03 19:14:20 -0300

    Update Estoqueinsu.java

--- Estoqueinsu.java
+++ Estoqueinsu.java
@@ -1,4 +1,4 @@
-// Classe de exceção personalizada para casos de estoque insuficiente
+
 public class Estoqueinsu extends Exception {
     public Estoqueinsu(String mensagem) {
         super(mensagem);
