2ec9cbc8e6dcde881ba8f2ed6095f0ca01a34b50
andreaffa21 <elcymercedes@hotmail.com>
2025-06-09 13:45:55 -0300

    Update and rename Bebida.java to BebidaAlcoolica.java

---
+++ BebidaAlcoolica.java
@@ -0,0 +1,15 @@
+public class BebidaAlcoolica extends Produto {
+    private String temperatura;
+
+    public BebidaAlcoolica(String nome, double preco, int volume, String temperatura) {
+        super(nome, preco, volume);
+        this.temperatura = temperatura;
+    }
+
+    public String getTemperatura() { return temperatura; }
+
+    @Override
+    public void exibirInfo() {
+        System.out.println("Alcoólica (" + temperatura + "): " + nome + " - R$" + preco + " - " + volume + "ml");
+    }
+}
