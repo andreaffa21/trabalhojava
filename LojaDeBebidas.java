e39d788b98e6b30a3b0edba4556ab6c16d961bd5
andreaffa21 <elcymercedes@hotmail.com>
2025-06-10 14:19:24 -0300

    Update LojaDeBebidas.java

--- LojaDeBebidas.java
+++ LojaDeBebidas.java
@@ -1,51 +1,46 @@
-
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
 public class LojaDeBebidas {
-    private ArrayList<Produto> estoque;
+    private ArrayList<Produto> catalogo;
+    private Map<String, Integer> quantidadesEstoque;
 
     public LojaDeBebidas() {
-        estoque = new ArrayList<>();
+        catalogo = new ArrayList<>();
+        quantidadesEstoque = new HashMap<>();
         carregarProdutos();
     }
 
     private void carregarProdutos() {
-        // Bebidas alcoólicas geladas
-        estoque.add(new BebidaAlcoolica("Cerveja", 5.99, 350, "Gelada"));
-        estoque.add(new BebidaAlcoolica("Vodka", 19.99, 500, "Gelada"));
-        estoque.add(new BebidaAlcoolica("Caipirinha", 14.99, 300, "Gelada"));
-        estoque.add(new BebidaAlcoolica("Catuaba", 9.99, 500, "Gelada"));
-        estoque.add(new BebidaAlcoolica("Ice", 8.50, 275, "Gelada"));
-
-        // Bebidas alcoólicas ambiente
-        estoque.add(new BebidaAlcoolica("Vinho", 29.99, 750, "Ambiente"));
-        estoque.add(new BebidaAlcoolica("Whisky", 89.90, 700, "Ambiente"));
-        estoque.add(new BebidaAlcoolica("Conhaque", 24.90, 500, "Ambiente"));
-        estoque.add(new BebidaAlcoolica("Rum", 32.00, 600, "Ambiente"));
-        estoque.add(new BebidaAlcoolica("Licor", 18.50, 500, "Ambiente"));
-
-        // Bebidas não alcoólicas geladas
-        estoque.add(new BebidaNaoAlcoolica("Refrigerante", 4.50, 600, "Gelada"));
-        estoque.add(new BebidaNaoAlcoolica("Água", 2.50, 500, "Gelada"));
-        estoque.add(new BebidaNaoAlcoolica("Chá Gelado", 3.90, 500, "Gelada"));
-        estoque.add(new BebidaNaoAlcoolica("Suco de Laranja", 4.80, 400, "Gelada"));
-        estoque.add(new BebidaNaoAlcoolica("Água com Gás", 3.00, 500, "Gelada"));
-
-        // Bebidas não alcoólicas ambiente
-        estoque.add(new BebidaNaoAlcoolica("Suco Natural", 6.00, 300, "Ambiente"));
-        estoque.add(new BebidaNaoAlcoolica("Água de Coco", 5.50, 330, "Ambiente"));
-        estoque.add(new BebidaNaoAlcoolica("Vitamina", 6.50, 400, "Ambiente"));
-        estoque.add(new BebidaNaoAlcoolica("Leite", 3.20, 500, "Ambiente"));
-        estoque.add(new BebidaNaoAlcoolica("Achocolatado", 4.20, 300, "Ambiente"));
+        adicionarProdutoAoEstoque(new BebidaAlcoolica("Cerveja", 5.99, 350, "Gelada"), 50);
+        adicionarProdutoAoEstoque(new BebidaAlcoolica("Vinho", 29.99, 750, "Ambiente"), 20);
+        adicionarProdutoAoEstoque(new BebidaAlcoolica("Vodka", 19.99, 500, "Gelada"), 30);
+        adicionarProdutoAoEstoque(new BebidaNaoAlcoolica("Refrigerante", 4.50, 600, "Gelada"), 100);
+        adicionarProdutoAoEstoque(new BebidaNaoAlcoolica("Água", 2.50, 500, "Gelada"), 150);
+        adicionarProdutoAoEstoque(new BebidaNaoAlcoolica("Suco Natural", 6.00, 300, "Ambiente"), 40);
     }
 
-    //classe Arraylist 
+    private void adicionarProdutoAoEstoque(Produto produto, int quantidade) {
+        catalogo.add(produto);
+        quantidadesEstoque.put(produto.getNome(), quantidade);
+    }
+    
+    public void verificarDisponibilidade(Produto produto, int quantidadeDesejada) throws EstoqueInsu {
+        int estoqueAtual = quantidadesEstoque.getOrDefault(produto.getNome(), 0);
+
+        if (quantidadeDesejada > estoqueAtual) {
+            throw new EstoqueInsu(
+                "Estoque insuficiente para '" + produto.getNome() + "'. Disponível: " + estoqueAtual
+            );
+        }
+    }
+
     public ArrayList<Produto> listarBebidas(String tipo, String temperatura) {
         ArrayList<Produto> listaFiltrada = new ArrayList<>();
-        for (Produto p : estoque) {
+        for (Produto p : catalogo) {
             if ((tipo.equals("alcoolica") && p instanceof BebidaAlcoolica) ||
                 (tipo.equals("naoalcoolica") && p instanceof BebidaNaoAlcoolica)) {
                 if (p instanceof BebidaAlcoolica ba && ba.getTemperatura().equalsIgnoreCase(temperatura)) {
@@ -55,16 +50,15 @@ public class LojaDeBebidas {
                 }
             }
         }
-    // exibi a informações em forma de lista ("alcoolica", "Gelada");
-        for (int i = 0; i < listaFiltrada.size(); i++) { 
+        for (int i = 0; i < listaFiltrada.size(); i++) {
             System.out.print(i + " - ");
-            listaFiltrada.get(i).exibirInfo();
+            listaFiltrada.get(i).exibirInfo(); // REQUISITO: Chamada Polimórfica
+            System.out.println("    (Estoque: " + quantidadesEstoque.get(listaFiltrada.get(i).getNome()) + ")");
         }
         return listaFiltrada;
     }
-
-//Oferece escolha da temperatura da bebida
-    public String selecionarTemperaturaPorIndice(Scanner sc) {
+    
+     public String selecionarTemperaturaPorIndice(Scanner sc) {
         System.out.println("Selecione a temperatura da bebida:");
         System.out.println("1 - Gelada");
         System.out.println("2 - Ambiente (Quente)");
@@ -77,7 +71,7 @@ public class LojaDeBebidas {
     }
 
     public Produto buscarProduto(String nome) {
-        for (Produto p : estoque) {
+        for (Produto p : catalogo) {
             if (p.getNome().equalsIgnoreCase(nome)) {
                 return p;
             }
