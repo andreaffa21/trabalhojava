e999d57f73058e5ab9e8f4f9963d8bd16cab12ff
andreaffa21 <elcymercedes@hotmail.com>
2025-06-10 18:50:48 -0300

    Update EstoqueInsu.java

--- EstoqueInsu.java
+++ EstoqueInsu.java
@@ -1,6 +1,9 @@
+
+//classe derivada de extend
+
 public class EstoqueInsu extends Exception {
 
     public EstoqueInsu(String mensagem) {
-        super(mensagem); // Passa a mensagem para a classe pai (Exception)
+        super(mensagem); // Passa a mensagem de excessão para o pai
     }
 }
